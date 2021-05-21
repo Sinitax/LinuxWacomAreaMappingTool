9e980fe87bf50f20b7585be73b14537378c15712
Louis Burda <quent.burda@gmail.com>
2021-05-21 19:26:59 +0200

    changed recover delay for device plug-in

--- scripts/tablet-reset.sh
+++ scripts/tablet-reset.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+SCRIPTPATH="$(dirname $(readlink -f "$0"))"
+cd "$SCRIPTPATH"
+
 REPOPATH="$(git rev-parse --show-toplevel)"
 
 # check if graphics tablet is already plugged in
 while read -r directory event filename; do
 	if [[ "$filename" =~ ^mouse[1-9]*[0-9]$ ]] && [ "$event" == "CREATE" ]; then
 		# wait for device to init
-		sleep 2
+		sleep 4
 		bash "$REPOPATH/scripts/recover.sh"
 	fi
 done < <(inotifywait -e create -q -m /dev/input)
